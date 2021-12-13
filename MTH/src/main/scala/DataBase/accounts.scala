96a8cd4c684d346d60d13b30b7397685d7afc873
Jaime Hinojos <jaimehinojos11@gmail.com>
2021-12-13 08:21:12 -0700

    Deposit into account

--- MTH/src/main/scala/DataBase/accounts.scala
+++ MTH/src/main/scala/DataBase/accounts.scala
@@ -6,6 +6,7 @@ import java.sql.SQLException
 import User.Customer
 
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+import scala.io.StdIn
 import scala.util.Random
 object accounts {
   val driver = "com.mysql.cj.jdbc.Driver"
@@ -30,13 +31,48 @@ object accounts {
       case e:Exception => println("Error")
     }
   }
-  def printAccountInfo(): Unit = {
+  def printAccountInfo(id:Long): Unit = {
+      try{
+        var resultSet = statement.executeQuery(s"SELECT * FROM accounts WHERE account_number = '$id';")
+        while ( resultSet.next() ) {
+          println(s"Account Number: ${resultSet.getString(1)}" +
+            s"\nAccount Type: ${resultSet.getString(2)}" +
+            s"\nAccount Balance: ${resultSet.getString(4)}")
+        }
+        var resultSet2 = statement.executeQuery(s"SELECT * FROM transactions WHERE account_id = '$id';")
+        if(!resultSet2.isBeforeFirst()){
+          println("You don't have any transactions")
+
+        }else{
+          println("Transactions")
+          println("Transaction Number, Transaction Type, Transaction Amount, Transaction Date Format")
+          while ( resultSet2.next() ) {
+            println(s"${resultSet2.getString(1)}                    " +
+              s"${resultSet2.getString(6)}              " +
+              s"$$${resultSet2.getString(4)}               " +
+              s"${resultSet2.getString(5)}            ")
+          }
+        }
+      }catch{
+        case e:Exception => println("Error occurred")
+      }
+  }
+  def depositIntoAccount(id:Long, amount:Double): Unit = {
+    try{
+      var resultSet = statement.executeQuery(s"SELECT account_balance FROM accounts WHERE account_number = '$id';")
+      resultSet.next()
+      var balance = resultSet.getString("account_balance").toDouble
+      balance += amount
+      statement.executeUpdate(s"UPDATE accounts SET account_balance = '$balance' WHERE account_number = '$id';")
+      statement.executeUpdate(s"INSERT INTO transactions (account_id, amount, transaction_type) VALUES ('$id', '$amount', 'Deposit');")
+      println("Money has been deposited")
+
+    }catch{
+      case e:Exception => println("Error occurred")
+    }
 
   }
-  def depositIntoAccount(): Unit = {
-
-  }
-  def transferr(): Unit ={
+  def transfer(): Unit ={
 
   }
   def getAccounts(id:Long):ArrayBuffer[Array[String]]= {
@@ -59,6 +95,26 @@ object accounts {
       case e:Exception => {println("Error")
         null
       }
+    }
+  }
+  def deleteAccount(id:Long): Unit ={
+    try{
+      var input = ""
+      println("Are you sure you want to delete this account(Y/N)? Any outstanding balance and information will be lost. ")
+      while(!input.equals("N")){
+        input = StdIn.readLine()
+        if(input.equals("Y")){
+          statement.executeUpdate(s"DELETE FROM accounts WHERE account_number = '$id';")
+          input = "N"
+        }else if(input.equals("N")){
+          println("Going back to main menu.")
+        }else{
+          print("Wrong input, try again: ")
+        }
+      }
+
+    }catch{
+      case e: Exception => println("Error Ocurred")
     }
   }
 
--- MTH/src/main/scala/Menus/homeMenu.scala
+++ MTH/src/main/scala/Menus/homeMenu.scala
@@ -31,32 +31,117 @@ object homeMenu {
           case e:Exception => println("Input type not valid")
         }
       }
-      print("MTH Bank \n1. Open an Account \n2. Manage Accounts\n3. Deposit\n4. Send/Transfer Money\n0. Sign Out" +
+      print("\n\nMTH Bank \n1. Open an Account \n2. Manage Accounts\n3. Deposit\n4. Send/Transfer Money\n0. Sign Out" +
       "\nPlease select an option: ")
       1
     }
     case 2 => {
       val accounts = connection.getAccounts(id)
-      var count = 1
-      println("\n\nAvailable accounts:")
-      for( x <- accounts ){
-        print(s"$count. ${x(0)} ${x(1)} Account"+"\n")
-        count+=1
+      if(accounts != null){
+        var count = 1
+        println("\n\nAvailable accounts:")
+        for( x <- accounts ){
+          print(s"$count. ${x(0)} ${x(1)} Account"+"\n")
+          count+=1
+        }
+        print("Please select an account: ")
+        var jump = false
+        var input = ""
+        var account_selected:Array[String] = Array()
+        while(jump != true){
+          try{
+            input = StdIn.readLine()
+            account_selected = accounts(input.toInt-1)
+            jump = true
+          }catch{
+            case e:Exception=> println("Wrong Input")
+          }
+        }
+        print(s"${account_selected(1)} Account ${account_selected(0)}  Options: \n1. Print Statement" +
+          s"\n2. View Account Information\n3. Delete Account\n0. Main Menu\nPlease select an option: ")
+        jump = false
+        while(jump!=true){
+          input = StdIn.readLine()
+          if(input.equals("1")){
+            println("print statement")
+            jump = true
+
+          }
+          else if(input.equals("2")){
+            println("\n\nAccount Info")
+            connection.printAccountInfo(account_selected(0).toLong)
+            jump = true
+          }
+          else if(input.equals("3")){
+            println("Delete account")
+            connection.deleteAccount(account_selected(0).toLong)
+            jump = true
+          }
+          else if(input.equals("0")){
+            println("Main Menu")
+            jump = true
+          }else{
+            print("\nNot a menu option.\nOptions: \n1. Print Statement\n2. View Account Info\n3. Delete Account\n0. Main Menu\nPlease select an option: ")
+          }
+        }
+        Thread.sleep(4000)
+      }else{
+        println("Back to main menu")
       }
-      print("Please select an account: ")
-      print("\nMTH Bank \n1. Open an Account \n2. Manage Accounts\n3. Deposit\n4. Send/Transfer Money\n0. Sign Out" +
+
+
+      print("\n\nMTH Bank \n1. Open an Account \n2. Manage Accounts\n3. Deposit\n4. Send/Transfer Money\n0. Sign Out" +
       "\nPlease select an option: ")
       1
     }
-    case 3 => {print("MTH Bank \n1. Open an Account \n2. Manage Accounts\n3. Deposit\n4. Send/Transfer Money\n0. Sign Out" +
+    case 3 => {
+      val accounts = connection.getAccounts(id)
+      if(accounts != null){
+        var count = 1
+        println("\n\nAvailable accounts:")
+        for( x <- accounts ){
+          print(s"$count. ${x(0)} ${x(1)} Account"+"\n")
+          count+=1
+        }
+        print("Please select an account: ")
+        var jump = false
+        var input = ""
+        var account_selected:Array[String] = Array()
+        while(jump != true){
+          try{
+            input = StdIn.readLine()
+            account_selected = accounts(input.toInt-1)
+            jump = true
+          }catch{
+            case e:Exception=> println("Wrong Input")
+          }
+        }
+
+        jump = false
+        while(jump!= true){
+          try{
+            print("Amount to Deposit: $")
+            input = StdIn.readLine()
+            connection.depositIntoAccount(account_selected(0).toLong, input.toDouble)
+            jump = true
+          }catch{
+            case e:Exception=> println("Error Ocurred")
+          }
+        }
+
+      }
+      else{
+        println("Back to main menu")
+      }
+      print("\n\nMTH Bank \n1. Open an Account \n2. Manage Accounts\n3. Deposit\n4. Send/Transfer Money\n0. Sign Out" +
       "\nPlease select an option: ")
       1
     }
-    case 4 => {print("MTH Bank \n1. Open an Account \n2. Manage Accounts\n3. Deposit\n4. Send/Transfer Money\n0. Sign Out" +
+    case 4 => {print("\n\nMTH Bank \n1. Open an Account \n2. Manage Accounts\n3. Deposit\n4. Send/Transfer Money\n0. Sign Out" +
       "\nPlease select an option: ")
       1
     }
-    case _ => {print("MTH Bank \n1. Open an Account \n2. Manage Accounts\n3. Deposit\n4. Send/Transfer Money\n0. Sign Out" +
+    case _ => {print("\n\nMTH Bank \n1. Open an Account \n2. Manage Accounts\n3. Deposit\n4. Send/Transfer Money\n0. Sign Out" +
       "\nPlease select an option: ")
       1
     }
